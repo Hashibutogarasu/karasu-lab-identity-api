6c544f64337d70dde51edd143dc8241c6a23a853
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-02-23 21:11:18 +0900

    fix: fixed api error

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
   SwaggerModule.setup("docs", app, documentFactory);
 
   const port = process.env.PORT ?? 3001;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port);
 }
 
 bootstrap();
