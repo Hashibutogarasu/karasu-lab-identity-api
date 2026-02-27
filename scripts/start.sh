26c0a42469039680c2ef8ebd74a278728325ac28
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-02-27 18:49:46 +0900

    chore: added update script

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,5 @@
 
 docker kill karasu-lab-identity-api
 docker rm karasu-lab-identity-api
+docker pull hashibutogarasu/karasu-lab-identity-api
 docker run -d -p 8083:8083 --env-file .env.production --name karasu-lab-identity-api hashibutogarasu/karasu-lab-identity-api
