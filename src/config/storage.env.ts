a92b583dbe932946d94f489cd4e04dcd6d91ce8e
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-02-27 18:16:08 +0900

    fix: fixed initialize error

--- src/config/storage.env.ts
+++ src/config/storage.env.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 dotenv.config();
 
 export const storageEnvSchema = z.object({
-  R2_ENDPOINT: z.string().url(),
+  R2_ENDPOINT: z.string(),
   R2_ACCESS_KEY_ID: z.string().min(1),
   R2_SECRET_ACCESS_KEY: z.string().min(1),
   R2_BUCKET: z.string().min(1),
