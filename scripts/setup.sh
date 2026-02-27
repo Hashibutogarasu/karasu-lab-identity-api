60f7ac057722c158aaa6bc361e1959643e8d2475
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-02-27 19:02:17 +0900

    feat: added watchtower-webhook

---
+++ scripts/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ -f .env.production ]; then
+    export $(cat .env.production | grep -v '#' | awk '/=/ {print $1}')
+fi
+
+docker kill watchtower-webhook || true
+docker rm watchtower-webhook || true
+docker pull containrrr/watchtower
+
+docker run -d \
+  --name watchtower-webhook \
+  --restart always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -p 8080:8080 \
+  -e WATCHTOWER_LIFECYCLE_HOOKS=true \
+  containrrr/watchtower \
+  --http-api-update \
+  --http-api-token "${WATCHTOWER_HTTP_API_TOKEN}" \
+  karasu-lab-identity-api
--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,8 @@
 docker kill karasu-lab-identity-api
 docker rm karasu-lab-identity-api
 docker pull hashibutogarasu/karasu-lab-identity-api
-docker run -d -p 8083:8083 --env-file .env.production --name karasu-lab-identity-api hashibutogarasu/karasu-lab-identity-api
+docker run -d -p 8083:8083 \
+  --env-file .env.production \
+  --name karasu-lab-identity-api \
+  --label com.centurylinklabs.watchtower.lifecycle.post-update="/app/scripts/setup.sh" \
+  hashibutogarasu/karasu-lab-identity-api
