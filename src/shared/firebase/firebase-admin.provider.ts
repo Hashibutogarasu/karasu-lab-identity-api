a423b63f26f4fad1872aef87f5d8ef150b23675e
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-02-28 22:30:18 +0900

    fix: fixed firebase initialize error

--- src/shared/firebase/firebase-admin.provider.ts
+++ src/shared/firebase/firebase-admin.provider.ts
@@ -1,20 +1,20 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import * as admin from 'firebase-admin';
-import { getFirestore } from 'firebase-admin/firestore';
+import { initializeApp, getApps, cert } from 'firebase-admin/app';
+import { getFirestore, Firestore } from 'firebase-admin/firestore';
 import { ConfigService } from '../config/config.service.js';
 
 @Injectable()
 export class FirebaseAdminProvider implements OnModuleInit {
-	private _firestore: admin.firestore.Firestore;
+	private _firestore: Firestore;
 
 	constructor(private readonly configService: ConfigService) {}
 
 	onModuleInit() {
 		const env = this.configService.getAll();
 
-		if (!admin.apps.length && env.FIREBASE_PROJECT_ID) {
-			admin.initializeApp({
-				credential: admin.credential.cert({
+		if (!getApps().length && env.FIREBASE_PROJECT_ID) {
+			initializeApp({
+				credential: cert({
 					projectId: env.FIREBASE_PROJECT_ID,
 					clientEmail: env.FIREBASE_CLIENT_EMAIL,
 					privateKey: env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
