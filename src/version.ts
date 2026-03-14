aa1e948aa8d93028501ed81c1fac44340f191477
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-03-14 21:50:37 +0900

    feat: added release-please automated versioning to version.ts

--- src/version.ts
+++ src/version.ts
@@ -1,20 +1,2 @@
-import { createRequire } from "module";
-
-const require = createRequire(import.meta.url);
-
-interface Package {
-	name: string;
-	version: string;
-}
-
-const getPkg = (): Package => {
-	try {
-		const pkg = require("../../package.json") as Package;
-		if (pkg.name === "karasu-lab-api") return pkg;
-		throw new Error();
-	} catch {
-		return require("../package.json") as Package;
-	}
-};
-
-export const packageVersion: string = getPkg().version;
+// x-release-please-version
+export const packageVersion = '3.1.0';
