84e3dde3997aaab527ca315f3b35ce2f09b855f6
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-05-02 15:15:10 +0900

    fix: fixed build errors

--- src/shared/repository/repository.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IRepository<T> {
-  create(id: string, data: Partial<T>): Promise<T>;
-  getById(id: string): Promise<T | null>;
-  update(id: string, data: Partial<T>): Promise<T>;
-  delete(id: string): Promise<void>;
-}
